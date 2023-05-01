1e817e90df5d1f787183b163609858926cbf6300
EvgenyTreskin <treskinei@gmail.com>
2023-05-01 17:00:24 +0300

    start commit

--- holiday-payment-calculator/holiday-payment-calculator/src/main/java/com/example/holidaypaymentcalculator/GreetingController.java
+++ holiday-payment-calculator/holiday-payment-calculator/src/main/java/com/example/holidaypaymentcalculator/GreetingController.java
@@ -1,11 +1,12 @@
 package com.example.holidaypaymentcalculator;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false,
