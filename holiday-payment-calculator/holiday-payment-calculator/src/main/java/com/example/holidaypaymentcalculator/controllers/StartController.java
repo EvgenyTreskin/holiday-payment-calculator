6bcbcdea427613d9dd76f34e119b5d683ea4bb95
EvgenyTreskin <treskinei@gmail.com>
2023-05-14 21:54:30 +0300

    add basic tests

--- holiday-payment-calculator/holiday-payment-calculator/src/main/java/com/example/holidaypaymentcalculator/controllers/StartController.java
+++ holiday-payment-calculator/holiday-payment-calculator/src/main/java/com/example/holidaypaymentcalculator/controllers/StartController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @Controller
 public class StartController {
 
     @GetMapping("/start")
-    public String start(HttpServletRequest request, Model model) {
+    public String start() {
         return "start";
     }
 
@@ -20,30 +21,75 @@ public class StartController {
     public String calculate(@RequestParam(value = "averageSalary", required = false) String as,
                             @RequestParam(value = "vacationDuration", required = false) String vd,
                             Model model) {
-
-        String message = "";
-        if (as.isEmpty()) {
-            message += "Average salary not specified! ";
+        String message;
+        try {
+            validateInput(as, vd);
+            double holidayPayment = getHolidayPayment(Double.parseDouble(as), Double.parseDouble(vd));
+            message = "Your holiday payment is: " + holidayPayment;
+        } catch (IOException e) {
+            message = e.getMessage();
         }
-        if (vd.isEmpty()) {
-            message += "Vacation duration not specified! ";
-        }
-
-        if (!vd.isEmpty() && !as.isEmpty()) {
-            double averageSalary = Double.parseDouble(as);
-            double vacationDuration = Double.parseDouble(vd);
-            if (averageSalary <= 0 || vacationDuration <= 0) {
-                message = "Average salary and Vacation duration must be above zero! ";
-            } else {
-                double holidayPayment = (averageSalary) * (vacationDuration);
-                message = "Your holiday payment is: " + holidayPayment;
-            }
-
-        }
-
         model.addAttribute("message", message);
         return "start";
     }
 
+    public void validateInput(String as, String vd) throws IOException {
+        String message = "";
+        Boolean valid = true;
+        if (!isPositiveNumber(as)) {
+            message += "Please specify average salary as number greater then zero.\n";
+            valid = false;
+        }
+        if (!isPositiveNumber(vd)) {
+            message += "Please specify duration vacation as number greater then zero.\n";
+            valid = false;
+        }
+        if (!valid) {
+            throw new IOException(message);
+        }
+    }
+
+    public double getHolidayPayment(double averageSalary, double vacationDuration) {
+        double scale = Math.pow(10, 3);
+        return Math.ceil(averageSalary * vacationDuration * scale)/scale;
+    }
+
+    public static boolean isPositiveNumber(String str) {
+        if (str.isEmpty()) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(str);
+            if (d <= 0) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 
 }
+//        if (as.isEmpty()) {
+//            message += "Average salary not specified!\n";
+//            valid = false;
+//        }
+//        if (vd.isEmpty()) {
+//            message += "Vacation duration not specified!\n";
+//            valid = false;
+//        }
+//        if (!as.isEmpty() && !vd.isEmpty()) {
+//            try {
+//                double averageSalary = Double.parseDouble(as);
+//                double vacationDuration = Double.parseDouble(vd);
+//                if (averageSalary <= 0 || vacationDuration <= 0) {
+//                    message = "Average salary and Vacation duration must be above zero!\n";
+//                    valid = false;
+//                } else {
+//                    double holidayPayment = getHolidayPayment(averageSalary, vacationDuration);
+//                    message = "Your holiday payment is: " + holidayPayment;
+//                }
+//            } catch (Exception e) {
+//                message = "Average salary and Vacation duration must be a number! ";
+//            }
+//        }
---
+++ holiday-payment-calculator/holiday-payment-calculator/src/test/java/com/example/holidaypaymentcalculator/controllers/StartControllerTest.java
@@ -0,0 +1,79 @@
+package com.example.holidaypaymentcalculator.controllers;
+
+import org.assertj.core.util.VisibleForTesting;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.ui.Model;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartControllerTest {
+    private final StartController startController = new StartController();
+    String invalidSalaryMassage = "Please specify average salary as number greater then zero.\n";
+    String invalidDurationMessage = "Please specify duration vacation as number greater then zero.\n";
+    @Mock
+    private Model model;
+
+
+    @Test
+    void startTest() {
+        assertEquals("start", startController.start());
+    }
+
+
+
+    @Test
+    void validateInputTrowsNullTest() {
+        Exception exception = assertThrows(IOException.class, () -> startController.validateInput("", ""));
+        assertTrue(exception.getMessage().contains(invalidSalaryMassage));
+        assertTrue(exception.getMessage().contains(invalidDurationMessage));
+    }
+    @Test
+    void validateInputTrowsCharsTest() {
+        Exception exception = assertThrows(IOException.class, () -> startController.validateInput("f", "/"));
+        assertTrue(exception.getMessage().contains(invalidSalaryMassage));
+        assertTrue(exception.getMessage().contains(invalidDurationMessage));
+    }
+    @Test
+    void validateInputTrowsNegativeTest() {
+        Exception exception = assertThrows(IOException.class, () -> startController.validateInput("-1", "-22.2"));
+        assertTrue(exception.getMessage().contains(invalidSalaryMassage));
+        assertTrue(exception.getMessage().contains(invalidDurationMessage));
+    }
+
+    @Test
+    void getHolidayPaymentTwoPositiveTest() {
+        assertEquals(14, startController.getHolidayPayment(
+                2.8, 5));
+    }
+    @Test
+    void getHolidayPaymentTwoNegativeTest() {
+        assertEquals(15, startController.getHolidayPayment(
+                -6, -2.5));
+    }
+    @Test
+    void getHolidayPaymentTwoZeroTest() {
+        assertEquals(0, startController.getHolidayPayment(
+                0, 0));
+    }
+    @Test
+    void getHolidayPaymentPositiveNegativeTest() {
+        assertEquals(-749.28, startController.getHolidayPayment(
+                33.6, -22.3));
+    }
+    @Test
+    void getHolidayPaymentZeroNegativeTest() {
+        assertEquals(-0.0, startController.getHolidayPayment(
+                0, -100));
+    }
+    @Test
+    void getHolidayPaymentZeroPositiveTest() {
+        assertEquals(0, startController.getHolidayPayment(
+                63.2, 0));
+    }
+
+}
