4372b75767d6adcf05f98fa52f6b40b0cf5d9204
EvgenyTreskin <treskinei@gmail.com>
2023-05-30 20:30:26 +0300

    add validateInputNotTrowsExceptionTest

--- holiday-payment-calculator/holiday-payment-calculator/src/test/java/com/example/holidaypaymentcalculator/controllers/StartControllerTest.java
+++ holiday-payment-calculator/holiday-payment-calculator/src/test/java/com/example/holidaypaymentcalculator/controllers/StartControllerTest.java
@@ -1,11 +1,9 @@
 package com.example.holidaypaymentcalculator.controllers;
 
-import org.assertj.core.util.VisibleForTesting;
-import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.ui.Model;
+
 
 import java.io.IOException;
 
@@ -17,19 +15,21 @@ class StartControllerTest {
     String invalidDurationMessage = "Please specify duration vacation as number greater then zero.\n";
 
 
-
     @Test
     void startTest() {
         assertEquals("start", startController.start());
     }
 
 
-
     @Test
-    void validateInputTrowsEmptyTest() {
+    void validateInputTrowsExceptionTest() {
         Exception exception = assertThrows(IOException.class, () -> startController.validateInput("", ""));
         assertTrue(exception.getMessage().contains(invalidSalaryMassage));
         assertTrue(exception.getMessage().contains(invalidDurationMessage));
+    }
+    @Test
+    void validateInputNotTrowsExceptionTest() {
+        assertDoesNotThrow(() -> startController.validateInput("4000", "3.5"));
     }
 
 
@@ -38,43 +38,52 @@ class StartControllerTest {
         assertEquals(14, startController.getHolidayPayment(
                 2.8, 5));
     }
+
     @Test
     void getHolidayPaymentTwoNegativeTest() {
         assertEquals(15, startController.getHolidayPayment(
                 -6, -2.5));
     }
+
     @Test
     void getHolidayPaymentTwoZeroTest() {
         assertEquals(0, startController.getHolidayPayment(
                 0, 0));
     }
+
     @Test
     void getHolidayPaymentPositiveNegativeTest() {
         assertEquals(-749.28, startController.getHolidayPayment(
                 33.6, -22.3));
     }
+
     @Test
     void getHolidayPaymentZeroNegativeTest() {
         assertEquals(-0.0, startController.getHolidayPayment(
                 0, -100));
     }
+
     @Test
     void getHolidayPaymentZeroPositiveTest() {
         assertEquals(0, startController.getHolidayPayment(
                 63.2, 0));
     }
+
     @Test
-    void isPositiveNumberTestPositive(){
+    void isPositiveNumberTestPositive() {
         assertTrue(startController.isPositiveNumber("30.3"));
     }
+
     @Test
     void isPositiveNumberTestNegative() {
         assertFalse(startController.isPositiveNumber("-2"));
     }
+
     @Test
     void isPositiveNumberTestZero() {
         assertFalse(startController.isPositiveNumber("0"));
     }
+
     @Test
     void isPositiveNumberTestChar() {
         assertFalse(startController.isPositiveNumber("h"));
